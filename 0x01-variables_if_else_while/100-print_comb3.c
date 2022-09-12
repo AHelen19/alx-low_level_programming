b52cf64f538d24e93630c88ea6e3eccb5dff1006
AHelen19 <ayelehelena16@gmail.com>
2022-09-12 07:04:19 -0700

    Dfsfs

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ *
+ *        and whether it is greater than 5, less than 6, or 0.
+ * Return: Always 0.
+ */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+			{
+				if (i != j)
+					if ((i * 10 + j) % 10 != 0)
+					{
+						putchar(ij);
+						putchar(',');
+						putchar(' ');
+					}
+				else
+					continue;
+			}
+	}
+	return (0);
+
+}
