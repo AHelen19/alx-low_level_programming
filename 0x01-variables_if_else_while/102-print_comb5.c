f75d124ab3548a5a80853ab189c51fc7b143a273
AHelen19 <ayelehelena16@gmail.com>
2022-09-12 10:25:37 -0700

    dfsdf

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ *      and whether it is greater than 5, less than 6, or 0.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit1, digit2, digit3, digit4;
+
+	for (digit1 = 0; digit1 < 10; digit1++)
+	{
+		for (digit2 = 0; digit2 < 10; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+				{
+					putchar((digit1 % 10) + '0');
+					putchar((digit2 % 10) + '0');
+					putchar(' ');
+					putchar((digit3 % 10) + '0');
+					putchar((digit4 % 10) + '0');
+					if (digit1 == 9 && digit2 == 8 && digit3 == 9)
+					break;
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
