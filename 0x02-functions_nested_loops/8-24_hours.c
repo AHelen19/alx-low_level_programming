e0ee6a743528e714e0053ea7d48a2b8dbfaea166
AHelen19 <ayelehelena16@gmail.com>
2022-09-14 13:13:39 -0700

    sdfsdf

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j;
+	for (i = 0 ; i < 24; i++)
+	{
+		for (j = 1; j < 60; j++)
+		{	
+			if (i < 10)
+				_putchar('0');
+			_putchar(i);
+			_putchar(':');
+			if (j < 10)	
+				_putchar('0');
+			_putchar(j);
+			_putchar('\n');
+	}
