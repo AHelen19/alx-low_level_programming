c1b19ea8b70cbf01120b0a75941b07f8acfcc99d
AHelen19 <ayelehelena16@gmail.com>
2022-09-14 10:40:26 -0700

    Sfsdfs

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.c"
+
+/**
+ * isalpha - functions
+ *
+ * returns 1 or 0
+ */
+int isalpha(char c)
+{
+	if ('A' <= c && c <= 'Z') || ('a' <= c && c <='z'
+		return (1);
+	else
+		return (0);
+}
