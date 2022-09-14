6be9db8bb1197bab8f2924ceae2091824600f918
AHelen19 <ayelehelena16@gmail.com>
2022-09-14 16:32:32 -0700

    multiple

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * times_table: is the function name*
+ *
+ */
+void times_table(void)
+{
+	int row, column;
+	for (row = 0 ; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			_putchar(row * column);
+			
+			if (row == 9 && column == 9)
+				break;
+			_putchar(',');
+			_putchar(' ');
+		}
