1fdc425780be2d1743bcc39e5a714277262bf1a0
AHelen19 <ayelehelena16@gmail.com>
2022-09-14 11:45:19 -0700

    sfsfsd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,12 +16,18 @@ int print_sign(int n)
 	int result;
 
 	if (n > 0)
+	{
 		_putchar('+');
-	else if (n == 0)
+		result = 1;
+	} else if (n == 0)
+	{
 		_putchar(0);
-	else
-		printf('-');
-
+		result = 0;
+	} else 
+	{	printf('-');
+		return = -1;
+	}
+	
 	return (result);
 }
 
