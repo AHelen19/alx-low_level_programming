03d81793ae7bcae29192e195021a0847ad28da5d
AHelen19 <ayelehelena16@gmail.com>
2022-09-14 18:35:49 -0700

    sfsdf

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-
+/**
+ *  main - List all nautral
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int num, sum;
+
 	num = 1;
 	sum = 0;
 	while (num < 1024)
 	{
 		if ((num % 3 == 0) || (num % 5 == 0))
 			sum = sum + num;
-		num--;
+		num++;
 	}
-	
+
 	printf("%d\n", sum);
 
 	return (0);
