ef6042f94926a15aa2d2b0c19469b15e4d482977
AHelen19 <ayelehelena16@gmail.com>
2022-09-15 13:00:03 -0700

    Sfsfsd

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,15 @@
+
+
+/**
+ *
+ *
+ *
+ */
+int findLargestPrim(n)
+{
+	int largest, prime;
+	
+	prime = 2;
+	while ((n / prime) != 1)
+	{
+			
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,6 +10,8 @@
  */
 int _isupper(int c);
 
+void print_number(int n);
+
 /*void print_alphabet(void);
 
 void print_alphabet_x10(void);
