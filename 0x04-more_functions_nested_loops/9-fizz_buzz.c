4c304f45a3f29e45ad6ae7fe2c109f0d7e936bec
AHelen19 <ayelehelena16@gmail.com>
2022-09-16 02:59:40 -0700

    ssfsd

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * mailfdn - Prints the numbers from 1-100, but for multiples of three,
+ * main - Prints the numbers from 1-100, but for multiples of three,
  *        Fizz is printed instead of the number, for multiples of five,
  *        Buzz, and for multiples of both three and five, FizzBuzz.
  *
