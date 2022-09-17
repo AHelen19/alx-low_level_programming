0f241721e21002ba2344369a6dc9dda33910d6c2
AHelen19 <ayelehelena16@gmail.com>
2022-09-16 18:24:08 -0700

    print most used

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,6 +14,7 @@ void print_number(int n);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 /*void print_alphabet(void);
 
 void print_alphabet_x10(void);
