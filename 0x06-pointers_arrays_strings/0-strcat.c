77a89d2f6a34a8d462558c49396554acd07cb1c0
AHelen19 <ayelehelena16@gmail.com>
2022-09-22 14:25:13 -0700

    str cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,6 @@
  * strcat - Concatenates the string pointed by @src, including the terminating
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
- *
  * Return: A pointer to the destination string @dest.
  */
 char *strcat(char *dest, const char *src)
@@ -18,4 +17,3 @@ char *strcat(char *dest, const char *src)
 
 	return (dest);
 }
-
